087af66ccc9100d489fce249c0d1735ece011734
Victor Lowther <victor.lowther@gmail.com>
2014-01-21 10:26:34 -0600

    Switch to having in_docker use opencrowbar base images.

--- bootstrap/ubuntu-12.04.lib.sh
+++ bootstrap/ubuntu-12.04.lib.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 locale-gen en_US.UTF-8
 update-locale LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
 install_ruby() {
     apt-get -y --force-yes update
     apt-get -y --force-yes dist-upgrade
