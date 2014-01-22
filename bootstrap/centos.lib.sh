dc593d40a6e3f97967df7dcce5fdd9f31c0b0f11
Victor Lowther <victor.lowther@gmail.com>
2014-01-22 05:54:58 -0600

    On Centos, Install ruby 2.0.0 using RVM.
    This installs ruby 2.0.0 to the crowbar user account on CentOS 6.5,
    letting us use a modern Rails for the Crowbar web app on without
    screwing up the system ruby.

--- bootstrap/centos.lib.sh
+++ bootstrap/centos.lib.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-install_ruby() {
-    yum -y update
-    yum -y install ruby ruby-devel rubygems curl
-}
+install_ruby() (
+    [[ -f /etc/sysconfig/i18n ]] || echo "LANG=$LANG" >/etc/sysconfig/i18n
+    if [[ ! -x /usr/bin/ruby ]]; then
+        yum -y upgrade
+        yum -y install http://mirrors.servercentral.net/fedora/epel/6/i386/epel-release-6-8.noarch.rpm || :
+        yum -y install gcc-c++ patch readline readline-devel zlib zlib-devel \
+            libyaml-devel libffi-devel openssl-devel make which \
+            install bzip2 autoconf automake libtool bison iconv-devel curl
+        yum -y install ruby ruby-devel rubygems
+    fi
+    [[ -L /dev/fd ]] || ln -sf /proc/self/fd /dev/fd
+    if [[ ! -d /home/crowbar/.rvm ]]; then
+        su -l -c 'curl -L get.rvm.io | bash -s stable' crowbar
+        su -l -c 'rvm install 2.0.0' crowbar
+        su -l -c 'rvm use 2.0.0 --default' crowbar
+    fi
+)
