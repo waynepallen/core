0812388657f1052312caf7fd3b3bcc164381557e
John H Terpstra <John_Terpstra@Dell.Com>
2014-02-05 16:08:01 -0600

    Add facility to build custom ruby-2.0.0 RPMS and install them.

--- bootstrap/centos.lib.sh
+++ bootstrap/centos.lib.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 install_ruby() (
+
+TMPDIR=/tmp/work
+RPMBUILD=$HOME/rpmbuild
+RUBYVER=ruby-2.0.0
+PKGTGT=$HOME/Ruby
+
+die () {
+    echo $1
+    exit -1
+}
+
     [[ -f /etc/sysconfig/i18n ]] || echo "LANG=$LANG" >/etc/sysconfig/i18n
     if [[ ! -x /usr/bin/ruby ]]; then
         yum -y upgrade
@@ -10,9 +21,59 @@ install_ruby() (
         yum -y install ruby ruby-devel rubygems
     fi
     [[ -L /dev/fd ]] || ln -sf /proc/self/fd /dev/fd
-    if [[ ! -d /home/crowbar/.rvm ]]; then
-        su -l -c 'curl -L get.rvm.io | bash -s stable' crowbar
-        su -l -c 'rvm install 2.0.0' crowbar
-        su -l -c 'rvm use 2.0.0 --default' crowbar
+
+    # Make sure we have needed ruby source files handy
+    if [[ -f "bootstrap/centos-ruby-src-files.sh" ]]; then
+        STUFF="bootstrap/centos-ruby-src-files.sh"
+    elif [[ -f "centos-ruby-src-files.sh" ]]; then
+        STUFF="centos-ruby-src-files.sh"
+    else
+        die "Can't find src files needed to build ruby-2.0.0 RPMs"
     fi
+
+    # Identify present location so we can return to it, create temp directory
+    PWD=`pwd` && mkdir -p $TMPDIR
+
+    # Prep destinfo, pull down ruby sources, create clean tarball
+    cd $TMPDIR && git clone https://github.com/ruby/ruby
+    cd ruby && git checkout origin/ruby_2_0_0
+    RUBYPL=`grep "^#define RUBY_PATCHLEVEL " version.h | awk '{print $3}'`
+    cd .. && mv ruby $RUBYVER-p$RUBYPL
+
+    # Now create the pristine tarball
+    tar cjf $RUBYVER-p$RUBYPL.tar.bz2 $RUBYVER-p$RUBYPL
+
+    # Create build tree, move ruby tarball into it, clean up mess
+    [[ -d $RPMBUILD ]] || mkdir -p $RPMBUILD/{SPECS,SOURCES} 
+    [[ -d $RPMBUILD/noarch ]] && rm -rf $RPMBUILD/noarch
+    [[ -d $RPMBUILD/x86_64 ]] && rm -rf $RPMBUILD/x86_64
+    mv $RUBYVER-p$RUBYPL.tar.bz2 $RPMBUILD/SOURCES/
+    cd $PWD && rm -rf $TMPDIR
+
+    . $STUFF
+
+    # Now check we are ready to go, then build the ruby packages and clean up
+    cd ../SPECS
+    grep $RUBYPL ruby.spec
+    [[ $? ]] || die "Ruby patchlevel substitution did not validate."
+    rpmbuild -ba --clean ruby.spec
+    [[ $? ]] || die "Ruby RPM Build failed"
+    CHKFILE=`ls $RPMBUILD/RPMS/x86_64/ruby-2.0.0*`
+    [[ $CHKFILE ]] || die "Can't find ruby-2.0.0 RPM - something went wrong!"
+
+    # Now move the built packages into the $PKGTGT directory, then clean up.
+    [[ -d $PKGTGT ]] || mkdir -p $PKGTGT
+    cd $RPMBUILD/RPMS
+    ( cd noarch; mv * $PKGTGT/ ) && ( cd x86_64; mv * $PKGTGT/ )
+    rm -rf $RPMBUILD
+    cd $PKGTGT
+    
+    # Remove installed ruby RPMS
+    yum -y delete ruby ruby-devel rubygems
+    # Install freshly built ruby-2.0.0 RPMS
+    yum install ruby ruby-devel rubygem-bigdecimal rubygem-io-console rubygem-json \
+        rubygem-psych ruby-libs 
+
+exit 0
+
 )
