38924b4853478dae3122ab65c276b56481d90e8e
Victor Lowther <victor.lowther@gmail.com>
2014-02-26 09:17:29 -0600

    Change the default logging level in production to info.
    Debug was way too verbose.

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -32,7 +32,7 @@ Crowbar::Application.configure do
   config.eager_load = true
 
   # See everything in the log (default is :info)
-  config.log_level = :debug
+  config.log_level = :info
 
   # Use a different logger for distributed setups
 
