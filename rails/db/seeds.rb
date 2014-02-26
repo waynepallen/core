f5e58ac63566ce954e6eaeabc473a685f9fc3334
Victor Lowther <victor.lowther@gmail.com>
2014-02-26 12:25:35 -0600

    Allow the framework to mount other rails engines.
    When the Crowbar web framework comes back up, do everything needed to
    load all the additional gems, mount the engines, and load the additional
    database seeds to finish the imports of any additional barclamps.

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,4 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-
-Barclamp.import('crowbar')
+source_path = File.expand_path(File.join(__FILE__,"../../.."))
+yml_blob = YAML.load_file(File.join(source_path,"crowbar.yml"))
+Barclamp.import("core",yml_blob,source_path)
+Dir.glob("/opt/opencrowbar/**/crowbar_engine/barclamp_*/db/seeds.rb") do |seedfile|
+  "#{seedfile.split('/')[-3].camelize}::Engine".constantize.load_seed
+end
