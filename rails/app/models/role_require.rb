d5387f6520c6b794042d27518d4c942768fe097d
Victor Lowther <victor.lowther@gmail.com>
2014-03-04 17:31:24 -0600

    Clean up the creation and maintenance of the role graph.
    * Create and extra attribute for RoleRequire that allows it to track the
      id of the Role it points to as a parent instead of always using the
      role name.
    * Arrange for Role and RoleRequire objects to resolve their parent roles
      whenever a new Role or RoleRequire object is created.
    * Instead of having jig client roles be outside the RoleRequire
      machinery, arrange for Jigs to create any needed RoleRequire objects
      when they are initialized.
    * Arrange for role cohorts for a role and its children to be
      recalculated and saved whenever a one of its RoleRequire objects gets
      resolved.
    * Have RoleRequire ensure that the overall Role graph stays acyclic.
      Attepmting to create a RoleRequire that would cause the Role graph to
      have a cycle will throw an exception.
    * Make Role.add_to_node_in_snapshot properly fail when you try to bind a
      Role that has an unresolved parent.

--- rails/app/models/role_require.rb
+++ rails/app/models/role_require.rb
@@ -16,15 +16,68 @@
 class RoleRequire < ActiveRecord::Base
 
   belongs_to      :role
-  has_one         :upstream,    :class_name => "Role", :foreign_key => "name", :primary_key => "requires"
-  alias_attribute :parent,      :upstream
+  belongs_to      :parent, class_name: Role, foreign_key: "required_role_id"
+  alias_attribute :upstream, :parent
 
-  after_save :update_cohort
+  before_create :enforce_acyclic
+
+  after_create :resolve_requires
+
+  def resolved?
+    !required_role_id.nil?
+  end
+
+  def resolve!
+    return if resolved?
+    Rails.logger.info("RoleRequire: Trying to resolve #{requires} to a Role for #{role.name}")
+    r = Role.find_by(name: self.requires)
+    return unless r
+    Rails.logger.info("RoleRequire: #{requires} resolves to role ID #{r.id}")
+    update_column(:required_role_id, r.id)
+    role.update_cohort
+  end
+
 
   private
 
-  def update_cohort
-    role.reset_cohort
+  # If there is a path in the graph from requires back to role_id,
+  # then allowing this RoleRequire to be created would make the role graph
+  # cyclic, and that is Not Allowed.
+  def enforce_acyclic
+    target_role = Role.find(role_id)
+    source_role = Role.find_by(name: requires)
+    Rails.logger.info("RoleRequire: Testing to see if having #{target_role.name} depend on #{requires} is OK")
+    # If our source role has not been added yet, then there is no way we can
+    # tell if adding this RoleRequires will result in a cyclic graph.
+    # However, in that case the cycle will be detected when the role that
+    # requires refers to adds its own RoleRequires, so we don't have to worry
+    # about it here in any case.
+    return true if source_role.nil?
+    # Iterate over all of the parents of source_role until we either:
+    # * Have no more parents to look at, in which case the graph is acyclic, or
+    # * We find target_role, in which case adding ourself would create a cyclic
+    #   graph.  If that happens, we will die with an exception.
+    parent_reqs = source_role.role_requires
+    until parent_reqs.empty? do
+      Rails.logger.info("RoleRequire: Examining #{parent_reqs.inspect}")
+      new_parent_reqs = []
+      parent_reqs.each do |parent_req|
+        if target_role.name == parent_req.requires
+          # Well, that is that.  Nothing to do but die.
+          raise("RoleRequire: Making #{target_role.name} depend on #{requires} makes the role graph cyclic through #{parent_req.role.name}")
+        elsif parent_req.resolved?
+          new_parent_reqs += parent_req.parent.role_requires
+        end
+      end
+      parent_reqs = new_parent_reqs.uniq
+    end
+    # Creating this RoleRequire will not make the craph cyclic,
+    # as far as we can tell.
+    true
+  end
+
+  def resolve_requires
+    resolve!
   end
 
 end
