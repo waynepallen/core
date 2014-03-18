723756b5873906add8b740dc810e10d11bb0f84d
Judd Maltin <judd@newgoliath.com>
2014-03-17 23:13:57 -0400

    adding missing chefignore files

---
+++ chef/cookbooks/bmc-nat/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Dell, Inc"
+maintainer_email  "crowbar@dell.com"
+license           "Apache 2.0"
+description       "Manages BMC-NET for Crowbar"
+long_description  "Manages BMC-NET for Crowbar"
+version           "0.1.0"
+
---
+++ chef/cookbooks/repos/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "Dell, Inc"
+maintainer_email  "crowbar@dell.com"
+license           "Apache 2.0"
+description       "repos"
+long_description  "repos"
+version           "0.1.0"
+
