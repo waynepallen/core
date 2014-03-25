d5d311373da705a107c096278470385c6d52b8f4
waynepallen <Wayne_Allen@dell.com>
2014-03-25 16:43:42 -0500

    remove non-functional code for now

--- script/roles/puppet-agent_sa/01-install-agent.sh
+++ script/roles/puppet-agent_sa/01-install-agent.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 
 if ! which puppet; then
-    if [[ -f /etc/redhat-release || -f /etc/centos-release ]]; then
-        # comment-out following line if you want the 2.7 puppet, otherwise we get 3.4.3
-        rpm -ivh https://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
-        yum -y makecache
-        yum install -y puppet
-        # rm control script - we don't have access to the stand-alone install...
-        /etc/init.d/puppet stop
-        rm -f /etc/init.d/puppet
-
-    elif [[ -d /etc/apt ]]; then
-        # enable repository for ubuntu precise
-        # comment-out following 3 lines if you want the 2.7 puppet, otherwise we get 3.4.3
-        wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
-        dpkg -i puppetlabs-release-precise.deb
-        apt-get -y update
-        apt-get -y --force-yes install puppet
-        # rm control script - we don't have access to the stand-alone install...
-        /etc/init.d/puppet stop
-        rm -f /etc/init.d/puppet
-
-    elif [[ -f /etc/SuSE-release ]]; then
-        zypper install -y -l puppet-common
-    else
-        die "Staged on to unknown OS media!"
-    fi
+    gem install puppet
+    puppet resource group puppet ensure=present
+    puppet resource user puppet ensure=present gid=puppet shell='/sbin/nologin'
 fi
 
 node_name=$(read_attribute "crowbar/puppet-agent-sa/name")
