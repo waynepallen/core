f0c500c7b90d750d965d0a5d6bf5e9ff4ec066f5
Victor Lowther <victor.lowther@gmail.com>
2014-03-28 13:50:41 -0500

    Fix the OS install roles to be more reliable.
    Instead of using a timeout and some hard-to-debug nohup magic to reboot
    a node into its OS install environment, just wait until the provisioner
    has finished doing its thing and reboot.

--- script/roles/provisioner-os-install/01-install-os.sh
+++ script/roles/provisioner-os-install/01-install-os.sh
@@ -3,6 +3,10 @@
 [[ -x /etc/init.d/crowbar_join.sh || -x /etc/init.d/crowbar ]] && exit 0
 
 set -x
-nohup /bin/bash -c 'command sleep 60; exec reboot' >/tmp/nohup.out &
-exit 0
+webserver=$(read_attribute "crowbar/provisioner/server/webserver")
+while true; do
+    curl -s -f -L -o /tmp/bootstate "$webserver/nodes/$(hostname -f)/bootstate" && \
+        [[ -f /tmp/bootstate && $(cat /tmp/bootstate) = *-install ]] && break
+    sleep 1
+done
 
