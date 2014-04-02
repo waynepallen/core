e5b9ef703f0b758b901482d4c5d01eed75674f3d
Judd Maltin <judd@newgoliath.com>
2014-04-02 17:42:26 -0400

    remove berkstest cookbook and create a kosher Berksfile

--- chef/cookbooks/berkstest/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'berkstest'
-maintainer       'Judd Maltin'
-maintainer_email 'judd@newgoliath.com'
-license          'Apache 2'
-description      'Installs/Configures berkstest'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
--- chef/cookbooks/berkstest/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: berkstest
-# Recipe:: default
-#
-# Copyright (C) 2014 Dell
-# 
-#  Apache 2 License
-#
